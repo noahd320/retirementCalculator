ad95244e19cf3e7d6db81435983508a1d1ea0aec
Noah Duran <46541422+noahd320@users.noreply.github.com>
2022-05-28 11:44:09 -0400

    Add files via upload

---
+++ noahDuran_Retire.py
@@ -0,0 +1,30 @@
+"""This simple Python Program collects user input
+and computes retirement planning advice
+Written by: 
+Noah Duran
+for LU's CSIS 110 course 
+B03
+Be sure you fully document your code with 
+comments like you see here."""
+
+#Asking for users Full Name, Current Age, Desired Retirement Age, Current Retirement Savinds, and Amount needed to retire
+#!/usr/bin/python
+fullName = input("What is your full name?: ") 
+currentAge = input("How old are you?: ")
+desiredRetirementAge = input("What age do you want to retire at?: ")
+currentRetirementSavings = input("How much do you currently have in savings for retirement?: ")
+desiredRetirementSavings = input("How much money do you need to retire?: ")
+
+#Calculate how many years until retirement age
+yearsRemaining = int(desiredRetirementAge) - int(currentAge)
+
+#Calculate how far away retirement savings goal is
+retirementMoney =  int(desiredRetirementSavings) - int(currentRetirementSavings)
+
+#Calculate savings amount per year to retire
+perYear = int(retirementMoney) / int(yearsRemaining)
+
+#Statement on years left until retirement and money needed to retire
+print (fullName + " you have", yearsRemaining, " until you can retire.")
+print ("You need", retirementMoney, " in order to retire.")
+print ("You should save", perYear, " per year to retire on time.")
