53e26d219300a1fb791e0699429a6c491d8d39f8
Noah Duran <46541422+noahd320@users.noreply.github.com>
2022-05-31 12:11:35 -0400

    Update noahDuran_Retire.py
    Changed to ensure that input cannot be anything other than int and that fullName prints with capitalization on the first letter of names.

--- noahDuran_Retire.py
+++ noahDuran_Retire.py
@@ -1,9 +1,21 @@
 #Asking for users Full Name, Current Age, Desired Retirement Age, Current Retirement Savinds, and Amount needed to retire
-fullName = input("What is your full name?: ") 
-currentAge = input("How old are you?: ")
-desiredRetirementAge = input("What age do you want to retire at?: ")
-currentRetirementSavings = input("How much do you currently have in savings for retirement?: ")
-desiredRetirementSavings = input("How much money do you need to retire?: ")
+fullName = input("What is your full name?: ")
+currentAge = int(input("How old are you?: "))
+ break
+  except: 
+    print ("Please input as a number")
+desiredRetirementAge = int(input("What age do you want to retire at?: "))
+ break
+  except: 
+    print ("Please input as a number")
+currentRetirementSavings = int(input("How much do you currently have in savings for retirement?: "))
+ break
+  except: 
+    print ("Please input as a number")
+desiredRetirementSavings = int(input("How much money do you need to retire?: "))
+ break
+  except: 
+    print ("Please input as a number")
 
 #Calculate how many years until retirement age
 yearsRemaining = int(desiredRetirementAge) - int(currentAge)
@@ -15,6 +27,6 @@ retirementMoney =  int(desiredRetirementSavings) - int(currentRetirementSavings)
 perYear = int(retirementMoney) / int(yearsRemaining)
 
 #Statement on years left until retirement and money needed to retire
-print (fullName + " you have", yearsRemaining, " until you can retire.")
+print (fullName[0].uppercase + " you have", yearsRemaining, " until you can retire.")
 print ("You need", retirementMoney, " in order to retire.")
 print ("You should save", perYear, " per year to retire on time.")
